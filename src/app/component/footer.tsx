c8a153800e0bbc20d4d306cb68b849698f2f5295
AsmaZahid <168186482+AshZahid@users.noreply.github.com>
2024-10-22 21:21:23 +0500

    Add files via upload

---
+++ src/app/component/footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer(){
+    return(
+       
+            <footer>
+            <div className="w-full bg-zinc-500 text-white">
+                <h3 className="text-sm font-bold text-zinc-800 text-center">Follow Us On</h3>
+                <div className="bg-zinc-500">
+                <a  href="https://www.instagram.com/" ><img src="https://th.bing.com/th/id/R.c5ad0581106cbcc8bb6e654d08362c0a?rik=gPjxnRd7g9Dclg&pid=ImgRaw&r=0"  className="h-10 w-10"/>Instagram</a> 
+                
+                <a href="https://www.facebook.com/" ><img src="https://th.bing.com/th/id/R.e790c25db5e52838040686612b1a732c?rik=uVuLX4sResGMKA&riu=http%3a%2f%2fpngimg.com%2fuploads%2ffacebook_logos%2ffacebook_logos_PNG19757.png&ehk=JL234rPBfx%2bf3tobhEVbPdNgJiWOhk251WyRwRAg940%3d&risl=&pid=ImgRaw&r=0"  className="h-10 w-10"/> Facebook</a>
+                
+                <h3 className="text-sm font-bold text-zinc-800">You can also join our Whatsapp group</h3>
+                
+                <a href="https://www.bing.com/"><img src="https://th.bing.com/th/id/OIP.z9suPf5LIaxSXx12DFv7wAHaHa?w=1200&h=1200&rs=1&pid=ImgDetMain"  className="h-10 w-10"/>Whatsapp</a>
+                </div>
+                <hr></hr>
+                <h3>California:Your privacy right | email us:WinColl56@gmail.com | Ph:034567032</h3>
+            </div>
+            </footer>
+        
+    )
+}
